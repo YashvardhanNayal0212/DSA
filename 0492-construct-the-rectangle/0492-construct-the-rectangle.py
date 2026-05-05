caee90b852ca5d6a0852b8f33eca31af26a5b6e8
YashvardhanNayal0212 <nayalyashvardhan02@gmail.com>
2026-05-06 02:42:59 +0530

    Time: 3 ms (30.62%), Space: 19.4 MB (17.51%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.py
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.py
@@ -1,12 +1,10 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
-        arr=[]
-        diff=l=w=0
-        for i in range (1,int(area**0.5)+1):
+        a=[]
+        m=float("inf")
+        for i in range(1,int(area**0.5 )+1):
             if area%i==0:
-                w=i
-                l=area//i
-                diff=min(diff,l,w)
-        arr.append(l)
-        arr.append(w)
-        return arr
+                diff=abs(i-area//i)
+                m=min(diff,m)
+                a=[area//i,i]
+        return a
