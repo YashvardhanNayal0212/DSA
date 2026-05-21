06bf7b1de8e4d673b0d12215c402fdd9b3c03935
YashvardhanNayal0212 <nayalyashvardhan02@gmail.com>
2026-05-21 17:56:24 +0530

    Time: 822 ms (93.07%), Space: 33.7 MB (35.58%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -5,8 +5,7 @@ class Solution:
         r=len(nums)-1
         m=0
         while l<=r:
-            s=nums[l]+nums[r]
-            m=max(m,s)
+            m=max(m,nums[l]+nums[r])
             l+=1
             r-=1
         return m
