f1430359ddb3ed8554879ed1eecab7108d3dd481
YashvardhanNayal0212 <nayalyashvardhan02@gmail.com>
2026-05-25 22:10:08 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.25%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        count=n=0
+        for i in range(len(s)):
+            if s[i]==" ":
+                count+=1
+                if count==k:
+                    return s[:i]
+        return s
+        
